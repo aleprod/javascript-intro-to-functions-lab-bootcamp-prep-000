20b674d1f1ad5e9140a2ca7bd85710e5761502ab
Alejandra Rodriguez <ale.rodriguez871@gmail.com>
2017-12-12 06:11:43 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string){
+  return string.toUpperCase();
+}
+function whisper(string){
+  return string.toLowerCase();
+}
+function logShout(string){
+  console.log(string.toUpperCase);
+  return spy();
+}
+function logWhisper(string){
+  console.log(string.toLowerCase);
+}
+function sayHiToGrandma(){
+  if (){
+
+  } else if {
+
+  } else{
+
+  }
+}
+/*
+shout.toUpperCase("hello");
+whisper.toLowerCase("hello");
+logShout.toUpperCase("HELLO");
+spy("Hello");
+logWhisper.toLowerCase("hello");
+sayHiToGrandma.toMixedCase("I love you, Grandma");
+
+*/
